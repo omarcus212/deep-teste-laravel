aefe72ffe152c7aac0a8700f5d8c1daeaaed7768
omarcus212 <marcusvinniciusouza@gmail.com>
2026-02-13 20:22:08 -0300

    fix: remove import css vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/user/profile.css', 'resources/js/user/profile.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
